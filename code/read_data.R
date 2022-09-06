3755c0d79d6edb23c58fa3e5a9e12604e4339ef8
Schnatz <janluca.schnatz@gmx.de>
2022-09-06 12:25:00 +0200

    make writing the data to .rds files to be more dry

--- code/read_data.R
+++ code/read_data.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(vroom)
 library(here)
+library(glue)
 
+# motor activity data
 motor_activity_data <- here("data/raw") %>%
   list.files(
     pattern = "(condition|control).*\\.csv$",
@@ -21,19 +23,16 @@ motor_activity_data <- here("data/raw") %>%
     into = c("id_group", "id_participant"),
     sep = "_"
   ) %>%
-  mutate(id_participant = as.numeric(id_participant),
-         id_participant = if_else(id_group == "condition", id_participant + 32, id_participant),
-         id_participant = as.character(id_participant),
-         id_participant = str_pad(id_participant, pad = "0", width = 2)) %>%
- relocate(id_participant, .before = everything()) %>%
+  mutate(
+    id_participant = as.numeric(id_participant),
+    id_participant = if_else(id_group == "condition", id_participant + 32, id_participant),
+    id_participant = as.character(id_participant),
+    id_participant = str_pad(id_participant, pad = "0", width = 2)
+  ) %>%
+  relocate(id_participant, .before = everything()) %>%
   arrange(id_participant)
 
-write_rds(
-  motor_activity_data,
-  file = here("data/processed", "motor_activity_data.rds"),
-  compress = "gz"
-  )
-
+# madrs data
 madrs_data <- here("data/raw") %>%
   list.files(
     pattern = ".*\\.csv$",
@@ -45,10 +44,12 @@ madrs_data <- here("data/raw") %>%
     into = c("id_group", "id_participant"),
     sep = "_"
   ) %>%
-  mutate(id_participant = as.numeric(id_participant),
-         id_participant = if_else(id_group == "condition", id_participant + 32, id_participant),
-         id_participant = as.character(id_participant),
-         id_participant = str_pad(id_participant, pad = "0", width = 2)) %>%
+  mutate(
+    id_participant = as.numeric(id_participant),
+    id_participant = if_else(id_group == "condition", id_participant + 32, id_participant),
+    id_participant = as.character(id_participant),
+    id_participant = str_pad(id_participant, pad = "0", width = 2)
+  ) %>%
   mutate(
     gender = factor(gender, levels = c(1, 2), labels = c("female", "male")),
     age = factor(age),
@@ -62,23 +63,33 @@ madrs_data <- here("data/raw") %>%
   rename(madrs_started = madrs1, madrs_stopped = madrs2) %>%
   arrange(id_participant)
 
-write_rds(
-  madrs_data,
-  file = here("data/processed", "madrs_data.rds"),
-  compress = "gz"
-)
-
-combined_data <- 
-inner_join(
-  motor_activity_data, 
-  madrs_data,
-  by = c("id_participant", "id_group")
+# combined data
+combined_data <-
+  inner_join(
+    motor_activity_data,
+    madrs_data,
+    by = c("id_participant", "id_group")
   ) %>%
-  mutate(id_group = factor(id_group, levels = c("control", "condition")),
-         id_participant = factor(id_participant))
+  mutate(
+    id_group = factor(id_group, levels = c("control", "condition")),
+    id_participant = factor(id_participant)
+  )
 
-write_rds(
-  combined_data,
-  file = here("data/processed", "combined_data.rds"),
-  compress = "gz"
-)
+# save all data as seperate .rds files
+list(
+  motor_activity_data,
+  madrs_data,
+  combined_data
+) %>%
+  set_names(
+    "motor_activity_data",
+    "madrs_data",
+    "combined_data"
+  ) %>%
+  map2(
+    .x = ., .y = names(.),
+    ~ write_rds(.x,
+      file = here("data/processed", glue("{.y}.rds")),
+      compress = "gz"
+    )
+  )
